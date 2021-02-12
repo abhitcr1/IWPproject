22b79722cb5fa00b2a4331e5e04a4587c71dd852
Anurag Verma <anu.anuragverma@gmail.com>
2021-02-12 15:12:21 +0530

    New Design

---
+++ home/main.js
@@ -0,0 +1,19 @@
+navigator.geolocation.getCurrentPosition(
+    (data) => {
+        $("#weather").text("Loading....");
+        const lat = data.coords.latitude;
+        const long = data.coords.longitude;
+        $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/forecast?units=metric&APPID=222905522bd2c16bfe46234ced76263f&lat=${lat}&lon=${long}`,
+            method: "GET",
+            success: function (data) {
+                $("#weather").html(
+                    `<div class="d-flex"><img src="https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png" alt="${data.list[0].weather[0].icon}" crossorigin="anonymous" class="weather-img" /><div class="px-1 d-flex flex-column align-items-start"><span class="fs-7">${data.city.name}</span><span class="fw-normal pt-1">${data.list[0].main.temp} °C, ${data.list[0].weather[0].main}</span></div></div>`
+                );
+            },
+        });
+    },
+    (error) => {
+        $("#weather").text("Permission Denied for Location");
+    }
+);
