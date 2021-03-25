cc40e7569d0f8985b3695e2d18a703ed0bb650a1
Abhit Anand <abhitcr1@gmail.com>
2021-03-26 03:47:57 +0530

    date icker and scrollbar

--- home/main.js
+++ home/main.js
@@ -17,3 +17,24 @@ navigator.geolocation.getCurrentPosition(
         $("#weather").text("Permission Denied for Location");
     }
 );
+$("#basicDate").flatpickr({
+    enableTime: true,
+    dateFormat: "F, d Y H:i"
+});
+
+$("#rangeDate").flatpickr({
+    mode: 'range',
+    dateFormat: "Y-m-d"
+});
+
+$("#timePicker").flatpickr({
+    enableTime: true,
+    noCalendar: true,
+    time_24hr: true,
+    dateFormat: "H:i",
+});
+
+$(".resetDate").flatpickr({
+    wrap: true,
+    weekNumbers: true,
+});
