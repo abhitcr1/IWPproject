de950452145462c619d0a38a8c93a2f1aa0b2012
Hritam Dutta <hritamloyola10@gmail.com>
2021-03-27 21:04:01 +0530

    Emojis Added

--- inside/inmain.js
+++ inside/inmain.js
@@ -1,15 +1,20 @@
 function toggle() {
-    document.getElementById("title").contentEditable = true;
-    document.getElementById("mood").contentEditable = true;
-    document.getElementById("txt").contentEditable = true;
-    document.getElementById("editbtn").style.display= "none";
-    document.getElementById("savebtn").style.display= "initial";
-}
-
-function restore() {
-    document.getElementById("title").contentEditable = false;
-    document.getElementById("mood").contentEditable = false;
-    document.getElementById("txt").contentEditable = false;
-    document.getElementById("savebtn").style.display= "none";
-    document.getElementById("editbtn").style.display= "initial";
+    var x = new Date(document.lastModified);
+    var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    var days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    var d= days[x.getDay()];                    
+    var mnt = months[x.getMonth()];
+    var dt= x.getDate();
+    var yr= x.getFullYear();
+    var h= x.getHours();
+    var m= x.getMinutes();
+    if (h > 1 && h < 12)
+        var t= h + ":" + m + " am";
+    else if (h == 12)
+        var t= 12 + ":" + m + " pm";
+    else if (h > 12)
+        var t= (h-12) + ":" + m + " pm";
+    else
+        var t= 12 + ":" + m + " am";  
+    document.getElementById("update").innerHTML= "Last updated on " + d + " " + mnt + " " + dt + ", " + yr + " | " + t;
 }
