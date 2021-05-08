54c0cd6b3e1a048b3553a4c30661a3334227e220
Anurag Verma <anu.anuragverma@gmail.com>
2021-05-08 21:13:54 +0530

    🐛 Fix Crashing on edit route

--- server/src/routes/edit.js
+++ server/src/routes/edit.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+const AuthMiddleWare = require("../middleware/AuthMiddleWare");
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-    return res.render("index.html", { page: "edit" });
+router.get("/", AuthMiddleWare, (req, res) => {
+    jwt.verify(
+        req.cookies["token"],
+        process.env.SECRET,
+        async (err, decode) => {
+            if (err)
+                return res.render("index.html", {
+                    page: "edit",
+                    name: "Bruce Wayne",
+                });
+            else {
+                const user = await User.findById(decode._id);
+                return res.render("index.html", {
+                    page: "edit",
+                    name: user["name"],
+                });
+            }
+        }
+    );
 });
 
 module.exports = router;
