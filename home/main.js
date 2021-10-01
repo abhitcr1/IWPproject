d7ff0faf8e3a20a8f4ff3d58e153624aaa718935
Anurag Verma <55322425+whoanuragverma@users.noreply.github.com>
2021-10-01 09:50:54 +0530

    Update main.js

--- home/main.js
+++ home/main.js
@@ -23,24 +23,4 @@ $(".picker").flatpickr({
         console.log(selectedString);
     },
 });
-// $("#basicDate").flatpickr({
-//     enableTime: true,
-//     dateFormat: "F, d Y H:i"
-// });
 
-// $("#rangeDate").flatpickr({
-//     mode: 'range',
-//     dateFormat: "Y-m-d"
-// });
-
-// $("#timePicker").flatpickr({
-//     enableTime: true,
-//     noCalendar: true,
-//     time_24hr: true,
-//     dateFormat: "H:i",
-// });
-
-// $(".resetDate").flatpickr({
-//     wrap: true,
-//     weekNumbers: true,
-// });
